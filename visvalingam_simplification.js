ec19f5254af7d06339a0bba5c39ec49583eba0b5
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2018-04-29 15:30:30 +0300

    Heap implementation

--- visvalingam_simplification.js
+++ visvalingam_simplification.js
@@ -7,12 +7,14 @@ const Heap = require('heap');
  * @date 12 april 2018
  */
 
+/*
+ * @returns {Number} Lenght of a given line
+ */
 function computeLineLength(line) {
   let a = Math.pow(line[0][0] - line[1][0], 2);
   let b = Math.pow(line[0][1] - line[1][1], 2);
   return Math.sqrt(a + b);
 }
-
 
 /*
  * @param {Number[]} a Point a
@@ -31,43 +33,104 @@ function computeTriangleArea(triangle) {
 }
 
 /*
- * @returns {Number[][]} Polyline tripleat areas with coord indexes 
+ * @returns {Object}
  */
-function computePolylineTripletAreas(polyline) {
+function createPolylinePointEntry(area, index, leftIndex, rightIndex) {
+  return {
+    area: area,
+    index: index,
+    left: leftIndex,
+    right: rightIndex
+  }
+}
+
+/*
+ * @returns {Number}
+ */
+function computePolylineAreaByPointEntry(polyline, entry) {
+  let triangle = [
+    polyline[entry.index],
+    polyline[entry.left],
+    polyline[entry.right]
+  ];
+
+  return computeTriangleArea(triangle);
+}
+
+/* Make two given PointEntries neighbors
+ * @returns {undefined}
+ */
+function linkPointEntries(left, right) {
+  left.right = right.index;
+  right.left = left.index;
+}
+
+/*
+ * @returns {Boolean} True means area can be computed and updated,
+ *  false otherwise
+ */
+function updatePointEntryArea(polyline, entry) {
+  if (polyline[entry.left] && polyline[entry.right]) {
+    entry.area = computePolylineAreaByPointEntry(polyline, entry);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*
+ * @returns {Number[][]} Array of PointEntries for each point
+ *   in the given polyline
+ */
+function computePolylinePointEntries(polyline) {
   return polyline
     .map((coord, i) => {
-      if (i == 0 || i == polyline.length - 1) {
-        return [i, Infinity];
+      let isFirst = i == 0;
+      let isLast = i == (polyline.length - 1);
+      let left = isFirst ? undefined : i - 1;
+      let right = isLast ? undefined : i + 1;
+
+      let area;
+      if (isFirst || isLast) {
+        area = Infinity;
       } else {
-        return [
-          i,
-          computeTriangleArea([polyline[i - 1], coord, polyline[i + 1]])
-        ];
+        area = computeTriangleArea([polyline[left], coord, polyline[right]]);
       }
+
+      return createPolylinePointEntry(area, i, left, right);
     });
 }
 
 /*
- * @param {Number} percentage How many points leave
+ * @param {Number} percentage Number of points in the result polyline
+ *   given as a percentage of polyline points
  * @returns {Number[][]} polyline
  */
-function eliminatePoints(polyline, polylineWithAreas, percentage) {
-  //create heap
-  let heap = new Heap((a, b) => a[1] - b[1]);
-  for (let entry of polylineWithAreas) {
+function eliminatePoints(polyline, pointEntries, percentage) {
+  let deleted = {};
+  // We always leave two points
+  let howManyDelete = Math.min(pointEntries.length - 2, 
+    pointEntries.length - Math.floor(pointEntries.length * percentage));
+
+  let heap = new Heap((a, b) => a.area - b.area);
+  for (let entry of pointEntries) {
     heap.push(entry);
   }
 
-  let howManyDelete = Math.min(heap.size() - 2, heap.size() - Math.floor(heap.size() * percentage));
-  let toDelete = {};
-
-  for (let i = 0; i < howManyDelete; i++) {
+  while(howManyDelete-- > 0) {
     let entry = heap.pop();
-    toDelete[entry[0]] = true
+    let leftEntry = pointEntries[entry.left];
+    let rightEntry = pointEntries[entry.right];
+
+    linkPointEntries(leftEntry, rightEntry);
+    if (updatePointEntryArea(polyline, leftEntry)) { heap.updateItem(leftEntry); }
+    if (updatePointEntryArea(polyline, rightEntry)) { heap.updateItem(rightEntry); }
+
+    deleted[entry.index] = true;
   }
 
   return polyline
-    .filter((coord, i) => !toDelete[i]);
+    .filter((coord, i) => !deleted[i]);
 }
 
 /*
@@ -77,7 +140,7 @@ function eliminatePoints(polyline, polylineWithAreas, percentage) {
  */
 function visvalingamSimplification(polyline, percentage) {
   return eliminatePoints(
-    polyline, computePolylineTripletAreas(polyline), percentage);
+    polyline, computePolylinePointEntries(polyline), percentage);
 }
 
 // section: Tests
